6d24a90fd8f67d0f862f3695a11070a67a76ef72
abhishek-vocso <abhishek@vocso.com>
2024-06-08 03:00:33 +0530

    update

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -51,5 +51,5 @@ app.get('/points', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
